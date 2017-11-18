d6c1e614f9d2004f03976149982e8594d5234b72
ejbreen <ejbreen@umich.edu>
2017-11-17 19:03:17 -0500

    updating to use local directory

--- R/main.R
+++ R/main.R
@@ -1,14 +1,15 @@
-library("simstudy", lib.loc="~/R/win-library/3.4")
-library("ggplot2", lib.loc="~/R/win-library/3.4")
+install.packages("simstudy")
+install.packages("ggplot2")
+
 
 #https://cran.r-project.org/web/packages/simstudy/vignettes/simstudy.html
 
 #source('T:/Programing/IOE_413/TKA/R/TKA Parameters Input.R')
 #currently importing data directly into pop sim paramaters file
 
-source('T:/Programing/IOE_413/TKA/R/TKA population simulation parameters.R')
+source('R/TKA population simulation parameters.R')
 
-source('T:/Programing/IOE_413/TKA/R/TKA population simulation creation.R')
+source('R/TKA population simulation creation.R')
 
 #source('T:/Programing/IOE_413/TKA/R/TKA Calculations.R')
 
