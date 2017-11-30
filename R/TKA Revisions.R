d6dd7f8b957fc633b54bd2d6dcbc8eaee110d1ed
ejbreen <ejbreen@umich.edu>
2017-11-30 14:25:59 -0500

    blah

--- R/TKA Revisions.R
+++ R/TKA Revisions.R
@@ -4,7 +4,12 @@ T_rev_random_def = T_pop_def
 T_rev_causal_def = T_pop_def
 
 T_rev_random_def <- defData(T_rev_random_def,
-                            varname = '5yr_Revision_Rate',
+                            varname = '5yr_Rev_Rate',
+                            dist = 'binary',
+                            formula = .005)
+
+T_rev_causal_def <- defData(T_rev_causal_def,
+                            varname = '5yr_Rev_Rate',
                             dist = 'nonrandom',
-                            formula = '1')
+                            formula = 'round(1/(1+exp(-())))')
 
