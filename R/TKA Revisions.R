4417e70443651b78588af7e753d575b49fc53b01
ejbreen <ejbreen@umich.edu>
2017-11-30 14:26:50 -0500

    blah

--- R/TKA Revisions.R
+++ R/TKA Revisions.R
@@ -11,5 +11,5 @@ T_rev_random_def <- defData(T_rev_random_def,
 T_rev_causal_def <- defData(T_rev_causal_def,
                             varname = '5yr_Rev_Rate',
                             dist = 'nonrandom',
-                            formula = 'round(1/(1+exp(-())))')
+                            formula = 'round(1/(1+exp(-(1))))')
 
