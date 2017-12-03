276aff4667e4299d6fa1c330d341c0420853d46e
ejbreen <ejbreen@umich.edu>
2017-12-03 16:22:08 -0500

    commiting

--- basic_git_push.sh
+++ basic_git_push.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+# to run type source basic_git_push.sh into command line
+
 module load git
 
 echo adding all files to commit
 git add *
 
 echo commiting with generic message
-git commit -m "generic commit from flux"
+git commit -m "commiting"
 
 echo pushing commit to master
 git push
