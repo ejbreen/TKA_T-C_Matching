f28558044c73be9fa065b8491e3fcbe9e382cd9f
ejbreen <ejbreen@umich.edu>
2017-12-04 14:35:22 -0500

    cleaning up the timing data outout for the main time test

--- Python/TimeTest.py
+++ Python/TimeTest.py
@@ -26,7 +26,7 @@ Matches = pd.Series([   5,     5,   5,   5,   5,  3,   2,   2,   2],
                     index = T_n_sets)
 weights = np.ones(len(T_pop_full.columns))
 
-Timing_Cols = ['model', 'T_n', 'matches', 'Setup Time',
+Timing_Cols = ['model','data set', 'T_n', 'matches', 'Setup Time',
                'c1_t','c2_t','c3_t','c4_t', 'reps', 'Solve Time']
 Timing_Data_Pd = pd.DataFrame(0, index = list(range(Matches.sum()*len(D_sets))),
                               columns = Timing_Cols, dtype = np.float)
@@ -37,17 +37,16 @@ writer  = FNC.build_writer("timings_%s_%i-%i_treatments"%
 # timing data iterator
 td = 0
 for dataSet in D_sets:
-    Timing_Data_Pd["model"] = 0
     C_pop_full, T_pop_full = FNC.Import_DataSets(dataSet) 
     for T_n in T_n_sets:
         C_pop, T_pop = FNC.Shrink_pop(C_pop_full, T_pop_full, T_n)
         for matches in list(range(1, Matches[T_n]+1)):
-            Timing_Data_Pd.loc[td][1:3] = [T_n, matches]
+            Timing_Data_Pd.loc[td][0:4] = [0, dataSet, T_n, matches]
             
             #set up the model and return the timings of the different element's
             m, assign, setup_Time = FNC.Build_Model(model, C_pop, T_pop,
                                                     matches, weights)
-            Timing_Data_Pd.loc[td][3:8] = setup_Time
+            Timing_Data_Pd.loc[td][4:9] = setup_Time
             
             FNC.printMessage("Start Presolve")
             m.presolve()
@@ -56,7 +55,7 @@ for dataSet in D_sets:
             for rnd in list(range(rounds[T_n])):
                 m.optimize()
                 runTimes[rnd] = m.runtime
-            Timing_Data_Pd.loc[td][8:10] = [rounds[T_n], round(runTimes.mean(),5)]
+            Timing_Data_Pd.loc[td][9:11] = [rounds[T_n], round(runTimes.mean(),5)]
             
             td = td + 1
         print "Data set %i, treatment set size %i complete"%(dataSet, T_n)
@@ -72,17 +71,16 @@ Timing_Data_TD = pd.DataFrame(0, index = list(range(Matches.sum()*len(D_sets))),
                               columns = Timing_Cols, dtype = np.float)
 td = 0
 for dataSet in D_sets:
-    Timing_Data_TD["model"] = 1
     C_pop_full, T_pop_full = FNC.Import_DataSets(dataSet)
     for T_n in T_n_sets:
         C_pop, T_pop = FNC.Shrink_pop(C_pop_full, T_pop_full, T_n)
         for matches in list(range(1, Matches[T_n]+1)):
-            Timing_Data_TD.loc[td][1:3] = [T_n, matches]
+            Timing_Data_TD.loc[td][0:4] = [1, dataSet, T_n, matches]
             
             #set up the model and return the timings of the different element's
             m, assign, setup_Time = FNC.Build_Model(model, C_pop, T_pop,
                                                     matches, weights)
-            Timing_Data_TD.loc[td][3:8] = setup_Time
+            Timing_Data_TD.loc[td][4:9] = setup_Time
             
             FNC.printMessage("Start Presolve")
             m.presolve()
@@ -91,7 +89,7 @@ for dataSet in D_sets:
             for rnd in list(range(rounds[T_n])):
                 m.optimize()
                 runTimes[rnd] = m.runtime
-            Timing_Data_TD.loc[td][8:10] = [rounds[T_n], round(runTimes.mean(),5)]
+            Timing_Data_TD.loc[td][9:11] = [rounds[T_n], round(runTimes.mean(),5)]
             
             td = td + 1
         print "Data set %i, treatment set size %i complete"%(dataSet, T_n)
