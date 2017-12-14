2dcf2d1df9f4356e3b5733fe0ec94ce921ef4bd6
ejbreen <ejbreen@umich.edu>
2017-12-13 21:38:02 -0500

    regression work

--- Python/Regression_Timing.py
+++ Python/Regression_Timing.py
@@ -7,29 +7,36 @@ Created on Tue Dec 12 18:41:31 2017
 """
 
 import pandas as pd
-from sklearn import linear_model as linReg
+import statsmodels.api as sm
 
 import sys
 import os
 sys.path.append(os.path.abspath("Python/"))
 import FNC
 
+FNC.printMessage("starting timing regression")
+
 Timings = pd.read_excel("Data/timings_both_models_4-128_treatments.xlsx", 2)
 
-m_setup = linReg.LinearRegression()
-m_c1 = linReg.LinearRegression()
-m_c2 = linReg.LinearRegression()
-m_c3 = linReg.LinearRegression()
-m_c4 = linReg.LinearRegression()
-m_solve = linReg.LinearRegression()
+Timings = Timings.assign(sqrt_T_n = lambda x: x.T_n**.5)
+Timings = Timings.assign(T_n_sqrd = lambda x: x.T_n**2)
 
-Params = Timings[['model', 'T_n', 'matches']]
+Timings = Timings.rename(index = str, columns = {'Setup Time':'setup_time'})
+Timings = Timings.rename(index = str, columns = {'Solve Time':'solve_time'})
 
-Params = Params.assign(sqrt_T_n = lambda x: x.T_n**.5)
-Params = Params.assign(T_n_sqrd = lambda x: x.T_n**2)
+m_setup = sm.OLS.from_formula('setup_time ~ C(model) + T_n + matches + sqrt_T_n + T_n_sqrd', Timings)
+m_c1 = sm.OLS.from_formula('c1_t ~ C(model) + T_n + matches + sqrt_T_n + T_n_sqrd', Timings)
+m_c2 = sm.OLS.from_formula('c2_t ~ C(model) + T_n + matches + sqrt_T_n + T_n_sqrd', Timings)
+m_c3 = sm.OLS.from_formula('c3_t ~ C(model) + T_n + matches + sqrt_T_n + T_n_sqrd', Timings)
+m_c4 = sm.OLS.from_formula('c4_t ~ C(model) + T_n + matches + sqrt_T_n + T_n_sqrd', Timings)
+m_solve = sm.OLS.from_formula('solve_time ~ C(model) + T_n + matches + sqrt_T_n + T_n_sqrd', Timings)
 
-m_setup.fit(Params, Timings['Setup Time'])
-m_c1.fit(Params, Timings['c1_t'])
-m_c2.fit(Params, Timings['c2_t'])
-m_c3.fit(Params, Timings['c3_t'])
-m_c4.fit(Params, Timings['c4_t'])
+models = [m_setup, m_c1, m_c2, m_c3, m_c4, m_solve]
+
+r = m_setup.fit()
+results = [r, r, r, r, r, r]
+
+i=0
+for m in models:
+    results[i]=m.fit()
+    i=i+1
