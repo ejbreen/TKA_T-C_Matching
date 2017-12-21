4ee58f391362851279794385213bca9ff9091f38
ejbreen <ejbreen@umich.edu>
2017-12-21 11:41:13 -0500

    still working on getting julia set up

--- Julia/Optimization.jl
+++ Julia/Optimization.jl
@@ -2,19 +2,22 @@ using JuMP
 using Gurobi
 using DataArrays
 using DataFrames
+using CSV
 using Gadfly
+using Distances
+using PyCall
 
-T_pop_full = readcsv("T_pop.csv")
-C_pop_full = readcsv("C_pop.csv")
 
-covars = size(T_pop_full,2)-1
+T_pop_full = CSV.read("T_pop.csv")[:,2:end]
+C_pop_full = CSV.read("C_pop.csv")[:,2:end]
 
-T_pop = convert(DataFrame,T_pop_full[2:size(T_pop_full,1),2:size(T_pop_full,2)])
+covars = size(T_pop_full,2)
 
 function shrink_pop(T_pop_full, C_pop_full, T_n)
     T_pop = T_pop_full[1:T_n, :]
     C_pop = C_pop_full[1:T_n*30, :]
     return T_pop, C_pop
+end
 
 T_n = 16
 T_pop, C_pop = shrink_pop(T_pop_full, C_pop_full, T_n)
@@ -27,9 +30,10 @@ m = Model(solver = GurobiSolver())
 I = 1:T_n
 J = 1:T_n*30
 K = 1:covars
+weight = ones(covars)
 
 @variable(m, assign[I, J], Bin)
-@variable(m, z[k])
+@variable(m, z[K])
 
 @objective(m, Min, sum(assign.*dist)) + sum(weight[k]*z[k] for k=K))
 
